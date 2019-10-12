a694894060bc78ddd90159f2df17b51f871afa66
Christian Joel Martinez <xtian437@gmail.com>
2019-10-13 01:43:41 +0800

    Make navbar align with container

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="Navbar">
-		
 		<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-info">
+			<div class="container">
 			<router-link to="/" class="navbar-brand b">Book Borrowing Logger</router-link>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
 				aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,12 +15,6 @@
 						v-for="route in $router.options.routes"
 						class="nav-item"
 						v-bind:class="{active: isRouteActive(route)}">
-						<!-- <a 
-							class="nav-link" 
-							v-bind:href="'#' + route.path">
-								{{route.meta.label}} 
-								<span v-if="isRouteActive(route)" class="sr-only">(current)</span>
-						</a> -->
 						<router-link 
 							class="nav-link" 
 							:to="route.path">
@@ -28,26 +22,10 @@
 								<span v-if="isRouteActive(route)" class="sr-only">(current)</span>
 						</router-link>
 					</li>
-					<!-- <li class="nav-item">
-						<a class="nav-link" href="#/log">Log<span class="sr-only">(current)</span></a>
-					</li> -->
-					<!-- <li class="nav-item">
-						<a class="nav-link" href="#/summary">Summary</a>
-					</li> -->
-					<!-- <li class="nav-item">
-						<a class="nav-link" href="#">Pricing</a>
-					</li> -->
-					<!-- <li class="nav-item">
-						<a class="nav-link" href="#/">About</a>
-					</li> -->
 				</ul>
-				<!-- <form class="form-inline my-2 my-lg-0">
-					<input class="form-control mr-sm-2" type="text" placeholder="Search">
-					<button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-				</form> -->
+			</div>
 			</div>
 		</nav>
-
 	</div>
 </template>
 
